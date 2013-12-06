caab0a15925edaca7bcc10bbea7d0564b6f668bf
Joshua Rahm <joshua.rahm@colorado.edu>
2013-12-06 11:39:30 -0700

    renabled to lightpager.py

--- lightpager.py
+++ lightpager.py
@@ -4,17 +4,12 @@ from gi.repository import Gtk, Gdk, GLib, Pango, PangoCairo
 from cairo import Context, ImageSurface, RadialGradient, FORMAT_ARGB32
 
 import signal
-import sys
-import os
-import tempfile
-import threading
-import time
-
 import dbus
 import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
 
 import subprocess
+from optparse import OptionParser
 
 class DbusService(dbus.service.Object):
     def __init__( self, icon ) :
@@ -39,7 +34,7 @@ class DbusService(dbus.service.Object):
         self.m_icon.move_char( 'l' ) ;
 
 class TrayIcon :
-    def __init__( self ) :
+    def __init__( self, width=2, height=2 ) :
         self.m_icon = Gtk.StatusIcon()
         self.m_icon.set_visible( True )
        # self.m_icon.connect( 'popup-menu', self.on_showpopup )
@@ -48,8 +43,8 @@ class TrayIcon :
         self.m_inactive_color = ( 0.6, 0.6, 0.6 )
         self.m_padding = 3
 
-        self.m_rows = 3
-        self.m_cols = 3
+        self.m_rows = height
+        self.m_cols = width
 
         nworkspaces = self.m_rows * self.m_cols
         if self.get_num_workspaces() != nworkspaces:
@@ -57,23 +52,26 @@ class TrayIcon :
 
         self.repaint( self.get_current_workspace() )
 
-    def draw_status_icon( self, ncols, nrows, selected_workspace  ) :
-        pixels_per_square = int(64 / max(ncols,nrows))
+    def draw_status_icon( self, selected_workspace  ) :
+        pixels_per_square = int(64 / max(self.m_cols, self.m_rows))
 
         surface = ImageSurface( FORMAT_ARGB32, 64, 64 )
         ctx = Context( surface )
 
+        yoff = (64 - (pixels_per_square * self.m_rows)) / 2
+        xoff = (64 - (pixels_per_square * self.m_cols)) / 2
+
         counter = 0
-        for y in range( nrows ) :
-            for x in range( ncols ) :
-                print ('drawing rectangle')
+        for y in range( self.m_rows ) :
+            for x in range( self.m_cols ) :
+#                print ('drawing rectangle')
 
                 if counter == selected_workspace:
                     ctx.set_source_rgb( *self.m_active_color )
                 else :
                     ctx.set_source_rgb( *self.m_inactive_color )
 
-                ctx.rectangle( x * pixels_per_square, y * pixels_per_square,
+                ctx.rectangle( xoff + x * pixels_per_square, yoff + y * pixels_per_square,
                     pixels_per_square - self.m_padding,
                     pixels_per_square - self.m_padding )
                 counter += 1
@@ -86,7 +84,7 @@ class TrayIcon :
         )
 
     def repaint( self, workspace_num ):
-        pixbuf = self.draw_status_icon( self.m_rows, self.m_cols, workspace_num )
+        pixbuf = self.draw_status_icon( workspace_num )
         self.m_icon.set_from_pixbuf( pixbuf )
 
     def get_num_workspaces( self ):
@@ -102,15 +100,16 @@ class TrayIcon :
         return subprocess.call(["xdotool", "set_num_desktops", str(numdesktops)])
 
     def set_workspace( self, x, y ):
-        print ( "setting workspace to (%d,%d)\n" % (x,y) )
+#        print ( "setting workspace to (%d,%d)\n" % (x,y) )
         if x < 0:
             self.set_workspace( self.m_cols + x, y )
         elif y < 0:
             self.set_workspace( x, self.m_rows + y )
         else:
+#            print ( "(rows,cols) = (%d,%d)\n" % (self.m_rows,self.m_cols) )
             x = x % self.m_cols
             y = y % self.m_rows
-            nextn = y * self.m_rows + x
+            nextn = y * self.m_cols + x
             subprocess.call(["xdotool", "set_desktop", str(nextn)])
             self.repaint(nextn)
         # self.repaint( self.get_current_workspace() )
@@ -129,34 +128,18 @@ class TrayIcon :
             (x,y) = self.get_current_workspacexy()
             self.set_workspace( x + 1, y )
 
-
+def parseArgs():
+    parser = OptionParser()
+    parser.add_option( "--width", dest="ncols", default=2,
+        help="Set the number of workspaces in a row", metavar="N" )
+    parser.add_option( "--height", dest="nrows", default=2,
+        help="Set the number of workspaces in a column", metavar="N" )
+    (opts,args) = parser.parse_args()
+    return opts
 
 if __name__ == '__main__' :
-    icon = TrayIcon()
-
-    def take_input() :
-        filename = os.path.join("/tmp/", 'fbpager.com')
-
-        try:
-            os.remove( filename )
-        except OSError:
-            pass
-
-        try:
-            os.mkfifo( filename )
-        except:
-            print ( "Unable to open named pipe :-(" )
-        else:
-            fifo = open( filename, 'r' )
-            while True:
-                char = fifo.read(1)
-                if not char:
-                    break ;
-                print ( "read char " + str(char) )
-                icon.move_char( char )
-                    
-    thr = threading.Thread( target=take_input )
-    thr.start()
+    opts = parseArgs();
+    icon = TrayIcon( int(opts.ncols), int(opts.nrows) )
 
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     DBusGMainLoop(set_as_default=True)
